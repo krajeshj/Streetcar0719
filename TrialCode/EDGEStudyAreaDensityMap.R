e507bd6e2149ddac0b51da9d2bfd164317240d91
krajeshj <k.rajesh.j@gmail.com>
2016-08-06 21:30:40 -0700

    Adde a few scripts for Density plots

---
+++ TrialCode/EDGEStudyAreaDensityMap.R
@@ -0,0 +1,167 @@
+# This program analyzes the instruction trace of a Ultimate Random
+
+library(dplyr)
+library(RSQLite)
+library(tidyr)
+library(ggplot2)
+library(readr)
+library(stringr)
+library(scales)
+library(mixtools)
+library(readxl)
+library(ggmap)
+
+# examine the files 
+dir()
+setwd("/Users/rajesh/Desktop/Coursera/SpringBoardGithub/StreetCar0719")
+
+# read in the csv 
+stcar_edge<- read_csv("./streetcarbuffer_parcels/parcel_csv_050616/StreetCarParcels_EDGE.csv")
+
+
+glimpse(stcar_edge)
+View(stcar_edge)
+
+# Function to get data from google servers
+
+#define a function that will process googles server responses for us.
+getGeoDetails <- function(address){   
+  #use the gecode function to query google servers
+  geo_reply = geocode(address, output='all', messaging=TRUE, override_limit=TRUE)
+  #now extract the bits that we need from the returned list
+  answer <- data.frame(lat=NA, long=NA, accuracy=NA, formatted_address=NA, address_type=NA, status=NA)
+  answer$status <- geo_reply$status
+  
+  #if we are over the query limit - want to pause for an hour
+  # while(geo_reply$status == "OVER_QUERY_LIMIT"){
+  #   print("OVER QUERY LIMIT - Pausing for 1 hour at:") 
+  #   time <- Sys.time()
+  #   print(as.character(time))
+  #   Sys.sleep(60*60)
+  #   geo_reply = geocode(address, output='all', messaging=TRUE, override_limit=TRUE)
+  #   answer$status <- geo_reply$status
+  # }
+  
+  #return Na's if we didn't get a match:
+  if (geo_reply$status != "OK"){
+    return(answer)
+  }   
+  #else, extract what we need from the Google server reply into a dataframe:
+  answer$lat <- geo_reply$results[[1]]$geometry$location$lat
+  answer$long <- geo_reply$results[[1]]$geometry$location$lng   
+  if (length(geo_reply$results[[1]]$types) > 0){
+    answer$accuracy <- geo_reply$results[[1]]$types[[1]]
+  }
+  answer$address_type <- paste(geo_reply$results[[1]]$types, collapse=',')
+  answer$formatted_address <- geo_reply$results[[1]]$formatted_address
+  
+  return(answer)
+}
+
+
+
+
+
+
+
+stcar_edge <-mutate(stcar_edge, addr_to_geocode = (paste(ADDRNO, ADDRST, ADDRSF, "cincinnati, OH")))
+
+# Some ADDRSF are coded as PW and we need to change this to PKWY so that Google can understand this
+#Find the observations iwht "PW" in the ADDRSF field
+# Change this to PKWY
+stcar_edge$ADDRSF[c(which(stcar_edge$ADDRSF == "PW"))] <- "PKWY"
+
+# initialized 
+
+#initialise a dataframe to hold the results
+geocoded <- data.frame()
+# find out where to start in the address list (if the script was interrupted before):
+startindex <- 1
+
+# Start the geocoding process - address by address. geocode() function takes care of query speed limit.
+
+
+
+
+
+for (ii in seq(startindex, nrow(stcar_edge))){
+  print(paste("Working on index", ii, "of", nrow(stcar_edge)))
+  #query the google geocoder - this will pause here if we are over the limit.
+  result = getGeoDetails(stcar_edge$addr_to_geocode[ii]) 
+  print(result$status)     
+  result$index <- ii
+  #append the answer to the results file.
+  geocoded <- rbind(geocoded, result)
+  #save temporary results as we are going along
+  #saveRDS(geocoded, tempfilename)
+}
+
+
+stcar_edge<-bind_cols(stcar_edge, geocoded)
+
+
+myMap <- get_map(location="1208 Sycamore st, Cincinnati,OH", source="google", maptype="roadmap", crop=FALSE, zoom = 15)
+
+
+# Get a ggplot object
+CinciMap        <- ggmap(myMap) 
+CinciDensityMap <-(myMap)
+
+# provide  data to aestheic mappings
+CinciMap <- CinciMap + geom_point(aes( x = as.numeric(long), y = as.numeric(lat), alpha = 1.0, col = EXLUCODE, size= as.numeric(ACREDEED *10)), data= stcar_edge)
+
+print(CinciMap)
+
+# Added a layer for density 
+CinciDensityMap <- CinciMap + stat_density2d(
+                          aes(x = as.numeric(long), y = as.numeric(lat), fill = ..level..,
+                            alpha = ..level..),
+                          bins = 6, geom = "polygon", data = stcar_edge)
+ 
+print(CinciDensityMap)
+
+# Create a heat map of Land Values 
+#CinciLandVal <-  ggmap(myMap)
+
+ 
+#CinciLandVal <- CinciLandVal + geom_tile(data = stcar_edge,inherit.aes = FALSE,
+#                      aes(x = as.numeric(long), y = as.numeric(lat), alpha = MKTLND),
+#                      fill = "red") + theme(axis.title.y = element_blank(), axis.title.x = element_blank())
+
+
+#CinciLandVal
+
+
+ 
+
+
+ 
+# Data Layer
+# n <-(ggplot)
+# 
+# # Data + Aesthetics Mapping 
+# n <-ggplot(data,aes(x= data$Department.Title, y = data$Total.Payments, color=data$Job.Class.Title, alpha=0.4, size=data$Overtime.Pay))
+# 
+# # Data + aes + Geometries 
+# n <- n + geom_jitter() 
+# 
+# # Data + Aesthetic Mapping +  Geom + Facets
+# #n <- n + facet_grid(. ~ Department.Title ) 
+# 
+# #Data + Aesthetics + Geoms + Facets + Statistics
+# # ... add a linear regression model here
+# 
+# #Data + Aesthetics + Geoms + Facets + Statistics + Co-ordinates
+# #n <- n + scale_y_discrete(limits=c("00000","120000"), breaks=seq(00000,120000,10000))
+# n <- n + xlab("City of Los Angeles Departments")
+# n <- n + ylab("Total Pay")
+# 
+# #Data + Aesthetics + Geoms + Facets + Statistics + Co-ordinates + Theme 
+# n <- n + theme(axis.text.x = element_text(angle=305))
+# 
+# print(n)
+
+
+
+
+
